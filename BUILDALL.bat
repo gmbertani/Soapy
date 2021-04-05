09df7e56dc5a3de7ba0c5e771264a36f26cfa601
Giuseppe Massimo Bertani <gmbertani@users.sourceforge.net>
2021-04-05 22:47:22 +0200

    now can build rtlsdr, airspy and audio devices

--- BUILDALL.bat
+++ BUILDALL.bat
@@ -2,9 +2,15 @@ rem ----------------------------------------------------------------
 rem BUILDALL
 rem builds SoapySDR utilities, library and devices support libraries
 rem ----------------------------------------------------------------
+
 SET INSTALL_PREFIX="c:\program files\Soapy"
 
-rem GOTO SKIP
+rem for laziness, the libusb provided is the static library with its header
+rem so there is no .pc file and CMake is not able to detect it implicitly
+SET LIBUSB_INCLUDE_DIR="D:\Users\massimo\Documents\sviluppo\echoes-related\soapy\deps\include"
+SET LIBUSB_LIBRARY_DIRS="D:\Users\massimo\Documents\sviluppo\echoes-related\soapy\deps\lib64"
+SET LIBUSB_LIBRARIES="libusb-1.0.a"
+GOTO SKIP
 
 rem builds the unixem library firs
 cd UNIXem\build\mingw730
@@ -20,9 +26,9 @@ del /q CMakeCache.*
 echo WARNING: in case of error like "The C compiler gcc.exe is not able to compile a simple test program."
 echo try to disable the antivirus for some minutes.
 
-cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% ..
+cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX%  ..
 make clean
-make
+make 
 make install
 cd ..\..
 
@@ -30,11 +36,14 @@ cd ..\..
 cd librtlsdr
 rmdir /s /q CMakeFiles
 del /q CMakeCache.*
-cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% .
+cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -DLIBUSB_INCLUDE_DIR=%LIBUSB_INCLUDE_DIR% -DLIBUSB_LIBRARY_DIRS=%LIBUSB_LIBRARY_DIRS% -DLIBUSB_LIBRARIES=%LIBUSB_LIBRARIES% .
 make clean
-make
+make VERBOSE=1
 make install
 cd ..
+
+
+
 
 cd SoapyRTLSDR
 rmdir /s /q CMakeFiles
@@ -43,6 +52,43 @@ cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% .
 make clean
 make
 make install
-
 cd ..
 
+
+cd airspyone_host
+rmdir /s /q CMakeFiles
+del /q CMakeCache.*
+cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -DLIBUSB_INCLUDE_DIR=%LIBUSB_INCLUDE_DIR% -DLIBUSB_LIBRARY_DIRS=%LIBUSB_LIBRARY_DIRS% -DLIBUSB_LIBRARIES=%LIBUSB_LIBRARIES% .
+make clean
+make
+make install
+cd ..
+
+
+
+cd SoapyAirspy
+rmdir /s /q CMakeFiles
+del /q CMakeCache.*
+cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% .
+make clean
+make
+make install
+cd ..
+
+
+:SKIP
+
+
+cd SoapyAudio
+rmdir /s /q CMakeFiles
+del /q CMakeCache.*
+cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -DCMAKE_PREFIX_PATH=%INSTALL_PREFIX% .
+make clean
+make
+make install
+
+GOTO END 
+
+:END
+
+cd ..
--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Soapy
 SoapySDR distribution patched for build with MingGW under Windows (no MSYS neither Cygwin needed)
 
-**Note**: this is WORK IN PROGRESS. It aims to integrate Echoes (https://sourceforge.net/projects/echoes)
-with SoapySDR (https://github.com/pothosware/SoapySDR/wiki) and could even be left incomplete. It successfully
-builds and install in my Win10 pc by launching BUILDALL.bat that builds and installs soapySDR dll, its utilities
+##Notes
+1. this is WORK IN PROGRESS. It aims to integrate Echoes (https://sourceforge.net/projects/echoes)
+with SoapySDR (https://github.com/pothosware/SoapySDR/wiki) and could even be left incomplete. 
+
+It successfully builds and install in my Win10 pc by launching BUILDALL.bat that builds and installs soapySDR dll, its utilities
 and the drivers dll (only rtlsdr currently... work in progress!).
 
 To try it, remember to adjust the paths on the top of BUILDALL.bat first to your environment. On my PC I develop on
-drive D: and you probably won't.
+drive D: and you probably don't.
 
+2. The libUSB binary and libpthread present under deps/ has been compiled under MSYS2/mingw 64bit
 
-
+3. the UNIXem library supplies the glob and dlfnc apis missing in mingw
