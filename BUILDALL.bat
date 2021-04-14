eb32e018bea3165ec58deb4c999a5792a2c8d621
Giuseppe Massimo Bertani <gmbertani@users.sourceforge.net>
2021-04-14 19:00:55 +0200

    defined %make%

--- BUILDALL.bat
+++ BUILDALL.bat
@@ -4,6 +4,7 @@ rem builds SoapySDR utilities, library and devices support libraries
 rem ----------------------------------------------------------------
 @echo off
 SET INSTALL_PREFIX="D:\program files\Soapy"
+SET MAKE="C:\Qt\Tools\mingw730_64\bin\mingw32-make.exe"
 
 rem for laziness, the libusb provided is the static library with its header
 rem so there is no .pc file and CMake is not able to detect it implicitly
@@ -13,80 +14,80 @@ SET LIBUSB_LIBRARIES="libusb-1.0.a"
 
 rem builds the unixem library firs
 cd UNIXem\build\mingw730
-make clean
-make
+%make% clean
+%make%
 cd ..\..\..
 rem install not needed
 @echo "UNIXem done, please take a look above for errors before continuing"
 pause
 
 cd SoapySDR\build
-make clean
+%make% clean
 rmdir /s /q CMakeFiles
 del /q CMakeCache.*
 
 echo WARNING: in case of error like "The C compiler gcc.exe is not able to compile a simple test program."
 echo try to disable the antivirus for some minutes.
 cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX%  ..
-make 
-make install
+%make% 
+%make% install
 cd ..\..
 @echo "SoapySDR installed, please take a look above for errors before continuing"
 pause
 
 cd librtlsdr
-make clean
+%make% clean
 rmdir /s /q CMakeFiles
 del /q CMakeCache.*
 cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -DLIBUSB_INCLUDE_DIR=%LIBUSB_INCLUDE_DIR% -DLIBUSB_LIBRARY_DIRS=%LIBUSB_LIBRARY_DIRS% -DLIBUSB_LIBRARIES=%LIBUSB_LIBRARIES% .
-make VERBOSE=1
-make install
+%make% VERBOSE=1
+%make% install
 cd ..
 @echo "librtlsdr SDK installed, please take a look above for errors before continuing"
 pause
 
 cd SoapyRTLSDR
-make clean
+%make% clean
 rmdir /s /q CMakeFiles
 del /q CMakeCache.*
 cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% .
-make
-make install
+%make%
+%make% install
 cd ..
 @echo "SoapyRTLSDR support library installed, please take a look above for errors before continuing"
 pause
 
 cd airspyone_host
-make clean
+%make% clean
 rmdir /s /q CMakeFiles
 del /q CMakeCache.*
 cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -DLIBUSB_INCLUDE_DIR=%LIBUSB_INCLUDE_DIR% -DLIBUSB_LIBRARY_DIRS=%LIBUSB_LIBRARY_DIRS% -DLIBUSB_LIBRARIES=%LIBUSB_LIBRARIES% .
-make
-make install
+%make%
+%make% install
 cd ..
 @echo "airspyone_host SDK installed, please take a look above for errors before continuing"
 pause
 
 
 cd SoapyAirspy
-make clean
+%make% clean
 rmdir /s /q CMakeFiles
 del /q CMakeCache.*
 cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% .
-make
-make install
+%make%
+%make% install
 cd ..
 @echo "SoapyAirspy support library installed, please take a look above for errors before continuing"
 pause
 
 
 cd SoapyAudio
-make clean
+%make% clean
 rmdir /s /q CMakeFiles
 del /q CMakeCache.*
 cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -DCMAKE_PREFIX_PATH=%INSTALL_PREFIX% .
-make
-make install
+%make%
+%make% install
 cd ..
 @echo "SoapyAudio support library installed, please take a look above for errors before continuing"
 pause
