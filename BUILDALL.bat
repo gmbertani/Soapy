c038c4d26813b975722ac0f524d2b6ced33a5c8c
Giuseppe Massimo Bertani <gmbertani@users.sourceforge.net>
2021-04-16 00:32:13 +0200

    improved batch files

--- BUILDALL.bat
+++ BUILDALL.bat
@@ -1,93 +1,148 @@
-rem ----------------------------------------------------------------
-rem BUILDALL
-rem builds SoapySDR utilities, library and devices support libraries
-rem ----------------------------------------------------------------
-@echo off
+REM ----------------------------------------------------------------
+REM BUILDALL
+REM builds SoapySDR utilities, library and devices support libraries
+REM ----------------------------------------------------------------
+@ECHO off
 SET INSTALL_PREFIX="D:\program files\Soapy"
-SET MAKE="C:\Qt\Tools\mingw730_64\bin\mingw32-make.exe"
+SET make="D:\Qt\Tools\mingw730_64\bin\mingw32-make.exe"
 
-rem for laziness, the libusb provided is the static library with its header
-rem so there is no .pc file and CMake is not able to detect it implicitly
-SET LIBUSB_INCLUDE_DIR="D:\Users\massimo\Documents\sviluppo\echoes-related\soapy\deps\include"
-SET LIBUSB_LIBRARY_DIRS="D:\Users\massimo\Documents\sviluppo\echoes-related\soapy\deps\lib64"
+REM for laziness, the libusb provided is the static library with its header
+REM so there is no .pc file and CMake is not able to detect it implicitly
+SET LIBUSB_INCLUDE_DIR="D:\Users\massimo\Documents\sviluppo\ECHOes-related\soapy\deps\include"
+SET LIBUSB_LIBRARY_DIRS="D:\Users\massimo\Documents\sviluppo\ECHOes-related\soapy\deps\lib64"
 SET LIBUSB_LIBRARIES="libusb-1.0.a"
 
-rem builds the unixem library firs
-cd UNIXem\build\mingw730
+REM builds the unixem library first
+SET /P yn=Build UNIXem? (y/n): 
+IF %yn% EQU n GOTO skipUNIXem
+
+CD UNIXem\build\mingw730
 %make% clean
 %make%
-cd ..\..\..
-rem install not needed
-@echo "UNIXem done, please take a look above for errors before continuing"
-pause
+CD ..\..\..
+REM install not needed
+@ECHO "UNIXem done, please take a look above for errors before continuing"
+REM
 
-cd SoapySDR\build
+:skipUNIXem
+SET /P yn=Build SoapySDR? (y/n): 
+IF %yn% EQU n GOTO skipSoapySDR
+
+CD SoapySDR\build
 %make% clean
-rmdir /s /q CMakeFiles
-del /q CMakeCache.*
+RMDIR /s /q CMakeFiles
+DEL /q CMakeCache.*
 
-echo WARNING: in case of error like "The C compiler gcc.exe is not able to compile a simple test program."
-echo try to disable the antivirus for some minutes.
+ECHO WARNING: in case of error like "The C compiler gcc.exe is not able to compile a simple test program."
+ECHO try to disable the antivirus for some minutes.
 cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX%  ..
 %make% 
 %make% install
-cd ..\..
-@echo "SoapySDR installed, please take a look above for errors before continuing"
-pause
+CD ..\..
+@ECHO "SoapySDR installed, please take a look above for errors before continuing"
+REM
 
-cd librtlsdr
+:skipSoapySDR
+SET /P yn=Build librtlsdr? (y/n): 
+IF %yn% EQU n GOTO skiplibrtlsdr
+
+CD librtlsdr
 %make% clean
+RMDIR /s /q CMakeFiles
+DEL /q CMakeCache.*
+cd tests
 rmdir /s /q CMakeFiles
 del /q CMakeCache.*
+cd ..
 cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -DLIBUSB_INCLUDE_DIR=%LIBUSB_INCLUDE_DIR% -DLIBUSB_LIBRARY_DIRS=%LIBUSB_LIBRARY_DIRS% -DLIBUSB_LIBRARIES=%LIBUSB_LIBRARIES% .
 %make% VERBOSE=1
 %make% install
-cd ..
-@echo "librtlsdr SDK installed, please take a look above for errors before continuing"
-pause
+CD ..
+@ECHO "librtlsdr SDK installed, please take a look above for errors before continuing"
+REM
 
-cd SoapyRTLSDR
+:skiplibrtlsdr
+SET /P yn=Build SoapyRTLSDR? (y/n): 
+IF %yn% EQU n GOTO skipSoapyRTLSDR
+
+CD SoapyRTLSDR
 %make% clean
-rmdir /s /q CMakeFiles
-del /q CMakeCache.*
+RMDIR /s /q CMakeFiles
+DEL /q CMakeCache.*
 cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% .
 %make%
 %make% install
-cd ..
-@echo "SoapyRTLSDR support library installed, please take a look above for errors before continuing"
-pause
+CD ..
+@ECHO "SoapyRTLSDR support library installed, please take a look above for errors before continuing"
+REM
 
-cd airspyone_host
+:skipSoapyRTLSDR
+SET /P yn=Build airspyone_host? (y/n): 
+IF %yn% EQU n GOTO skipairspyone_host
+
+CD airspyone_host
 %make% clean
-rmdir /s /q CMakeFiles
-del /q CMakeCache.*
+RMDIR /s /q CMakeFiles
+DEL /q CMakeCache.*
 cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -DLIBUSB_INCLUDE_DIR=%LIBUSB_INCLUDE_DIR% -DLIBUSB_LIBRARY_DIRS=%LIBUSB_LIBRARY_DIRS% -DLIBUSB_LIBRARIES=%LIBUSB_LIBRARIES% .
 %make%
 %make% install
-cd ..
-@echo "airspyone_host SDK installed, please take a look above for errors before continuing"
-pause
+CD ..
+@ECHO "airspyone_host SDK installed, please take a look above for errors before continuing"
+REM
+
+:skipairspyone_host
+SET /P yn=Build SoapyAirspy? (y/n): 
+IF %yn% EQU n GOTO skipSoapyAirspy
 
 
-cd SoapyAirspy
+CD SoapyAirspy
 %make% clean
-rmdir /s /q CMakeFiles
-del /q CMakeCache.*
+RMDIR /s /q CMakeFiles
+DEL /q CMakeCache.*
 cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% .
 %make%
 %make% install
-cd ..
-@echo "SoapyAirspy support library installed, please take a look above for errors before continuing"
-pause
+CD ..
+@ECHO "SoapyAirspy support library installed, please take a look above for errors before continuing"
+REM
 
+:skipSoapyAirspy
+SET /P yn=Build rtAudio? (y/n): 
+IF %yn% EQU n GOTO skiprtAudio
 
-cd SoapyAudio
+CD rtAudio-5.1.0
 %make% clean
-rmdir /s /q CMakeFiles
+DEL /q *.vcxproj
+DEL /q *.filters
+DEL /q *.cmake
+RMDIR /s /q CMakeFiles
 del /q CMakeCache.*
+CD _build_
+DEL /q *.*
+RMDIR /s /q CMakeFiles
+RMDIR /s /q Testing
+cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% ..
+%make% VERBOSE=1
+%make% install
+CD ..\..
+@ECHO "rtAudio SDK installed, please take a look above for errors before continuing"
+REM
+
+:skiprtAudio
+SET /P yn=Build SoapyAudio? (y/n): 
+IF %yn% EQU n GOTO skipSoapyAudio
+
+CD SoapyAudio
+%make% clean
+RMDIR /s /q CMakeFiles
+DEL /q CMakeCache.*
 cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -DCMAKE_PREFIX_PATH=%INSTALL_PREFIX% .
 %make%
 %make% install
-cd ..
-@echo "SoapyAudio support library installed, please take a look above for errors before continuing"
-pause
+CD ..
+@ECHO "SoapyAudio support library installed, please take a look above for errors before continuing"
+REM
+
+:skipSoapyAudio
+
--- CLEANALL.bat
+++ CLEANALL.bat
@@ -1,6 +1,6 @@
 rem ----------------------------------------------------------------
-rem BUILDALL
-rem builds SoapySDR utilities, library and devices support libraries
+rem CLEANALL
+rem cleans SoapySDR utilities, library and devices support libraries
 rem ----------------------------------------------------------------
 @echo off 
 
@@ -8,45 +8,62 @@ SET INSTALL_PREFIX="d:\program files\Soapy"
 rem rmdir /s /q %INSTALL_PREFIX%
 rem GOTO SKIP
 
+SET make="D:\Qt\Tools\mingw730_64\bin\mingw32-make.exe"
+
 rem builds the unixem library first
 cd UNIXem\build\mingw730
-make clean
+%make% clean
 cd ..\..\..
 
 cd SoapySDR\build
-make clean
+%make% clean
 rmdir /s /q CMakeFiles
 del /q CMakeCache.*
+cd tests
+rmdir /s /q CMakeFiles
+del /q CMakeCache.*
+cd ..
 cd ..\..
 
 cd librtlsdr
-make clean
+%make% clean
 rmdir /s /q CMakeFiles
 del /q CMakeCache.*
 cd ..
 
 cd SoapyRTLSDR
-make clean
+%make% clean
 rmdir /s /q CMakeFiles
 del /q CMakeCache.*
 cd ..
 
 cd airspyone_host
-make clean
+%make% clean
 rmdir /s /q CMakeFiles
 del /q CMakeCache.*
 cd ..
 
 cd SoapyAirspy
-make clean
+%make% clean
 rmdir /s /q CMakeFiles
 del /q CMakeCache.*
 cd ..
 
-
+CD rtAudio-5.1.0
+%make% clean
+DEL /q *.vcxproj
+DEL /q *.filters
+DEL /q *.cmake
+RMDIR /s /q CMakeFiles
+del /q CMakeCache.*
+CD _build_
+DEL /q *.*
+RMDIR /s /q CMakeFiles
+RMDIR /s /q Testing
+cd ..\..
 
 cd SoapyAudio
-make clean
+%make% clean
 rmdir /s /q CMakeFiles
 del /q CMakeCache.*
 
